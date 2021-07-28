ceaed4ba7deea584f31705334fb776a167b8b963
Milad <khoshkaran.milad@gmail.com>
2021-07-28 19:40:25 +0200

    routes - chaptersRoute implemented

--- server/routes/chaptersRoutes.js
+++ server/routes/chaptersRoutes.js
@@ -1,4 +1,8 @@
 const express = require("express");
 const chaptersRoutes = express.Router();
 
+const { chapterInfo } = require("../middlewares/chapterInfoHandler");
+
+chaptersRoutes.get("/", chapterInfo);
+
 module.exports = { chaptersRoutes };
