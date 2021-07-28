f93992931592eaff771a3f0544acf882a22eb8e3
Milad <khoshkaran.milad@gmail.com>
2021-07-28 20:08:56 +0200

    routes - recipesRoutes implemented

--- server/routes/recipesRoutes.js
+++ server/routes/recipesRoutes.js
@@ -1,4 +1,12 @@
 const express = require("express");
 const recipesRoutes = express.Router();
 
+const {
+    recipeInfo,
+    recipesInCookBook,
+} = require("../middlewares/recipesHandler");
+
+recipesRoutes.get("/:recipe_name", recipeInfo);
+recipesRoutes.get("/:cookbook_id", recipesInCookBook);
+
 module.exports = { recipesRoutes };
