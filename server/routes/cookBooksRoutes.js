6678c4e0e99fd815561fcf347ca1712c511ec8c6
Milad <khoshkaran.milad@gmail.com>
2021-07-28 18:53:14 +0200

    routes - cookBookRoutes implemented

--- server/routes/cookBooksRoutes.js
+++ server/routes/cookBooksRoutes.js
@@ -1,4 +1,16 @@
 const express = require("express");
 const cookBooksRoutes = express.Router();
 
+const {
+    cookBookInfo,
+    createCookBook,
+    updateCookBookInfo,
+} = require("../middlewares/cookBooksRoutesHandler");
+
+cookBooksRoutes.get("/:user_id", cookBookInfo);
+
+cookBooksRoutes.post("/create_cookbook", createCookBook);
+
+cookBooksRoutes.put("/update_info", updateCookBookInfo);
+
 module.exports = { cookBooksRoutes };
