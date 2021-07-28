2e7bec832db895268d1fb28a4b7d26eb014a4654
CHRISTO GOGLER <christogogler@gmail.com>
2021-07-29 00:18:20 +0200

    add LInks

--- client/src/components/Registration.js
+++ client/src/components/Registration.js
@@ -1,11 +1,12 @@
 //components
 import RegistrationForm from "./forms/RegistrationForm";
-
+import { Link } from "react-router-dom";
 export default function Registration() {
     return (
         <div className="authentificationWrapper">
             <h1>Sign up and create your own community cookbook</h1>
             <RegistrationForm />
+            <Link to="/login">Login</Link>
         </div>
     );
 }
