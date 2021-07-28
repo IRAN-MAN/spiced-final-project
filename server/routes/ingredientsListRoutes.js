98010aecb55e419d372ee5af7a9d685777765541
Milad <khoshkaran.milad@gmail.com>
2021-07-28 20:02:02 +0200

    routes - ingredientList routes implemented

--- server/routes/ingredientsListRoutes.js
+++ server/routes/ingredientsListRoutes.js
@@ -1,4 +1,12 @@
 const express = require("express");
 const ingredientsListRoutes = express.Router();
 
+const {
+    getIngredientList,
+    addIngredientList,
+} = require("../middlewares/ingredientsListHandler");
+
+ingredientsListRoutes.get("/:recipe_id", getIngredientList);
+ingredientsListRoutes.post("/:recipe_id", addIngredientList);
+
 module.exports = { ingredientsListRoutes };
