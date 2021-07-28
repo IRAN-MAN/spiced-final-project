2fe6e8c11a2c085c9fbc1b65b3d4d31ff9f3e696
CHRISTO GOGLER <christogogler@gmail.com>
2021-07-29 01:50:52 +0200

    change markup, add basic style

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -3,7 +3,7 @@ import LoginForm from "./forms/LoginForm";
 import { Link } from "react-router-dom";
 export default function Login() {
     return (
-        <div>
+        <div className="authentificationWrapper">
             <h1>Login with your email!</h1>
             <LoginForm />
             <Link to="/">Sign up</Link>
