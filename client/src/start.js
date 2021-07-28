d83fa73c043659bc507d149d9380d91cec882e21
CHRISTO GOGLER <christogogler@gmail.com>
2021-07-28 15:16:08 +0200

    add imports, add loggedIn/loggedOut

--- client/src/start.js
+++ client/src/start.js
@@ -1,7 +1,29 @@
 import ReactDOM from "react-dom";
+import reducer from "./redux/reducer";
 
-ReactDOM.render(<HelloWorld />, document.querySelector("main"));
+// redux & redux devtools
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+import reduxPromise from "redux-promise";
+import { composeWithDevTools } from "redux-devtools-extension";
+const store = createStore(
+    reducer,
+    composeWithDevTools(applyMiddleware(reduxPromise))
+);
 
+//components
+import Welcome from "./components";
+import App from "./components";
+
+const loggedIn = true;
+
+if (loggedIn) {
+    //logged-in
+    ReactDOM.render(<App />, document.querySelector("main"));
+} else {
+    // logged-out
+    ReactDOM.render(<Welcome />, document.querySelector("main"));
+}
 function HelloWorld() {
     return <div>Hello, World!</div>;
 }
