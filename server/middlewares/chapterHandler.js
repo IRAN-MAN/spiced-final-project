daf1161c6da4a7f1d9c2f7f56def841256967415
Milad <khoshkaran.milad@gmail.com>
2021-07-29 23:00:57 +0200

    handler - getchapters added

---
+++ server/middlewares/chapterHandler.js
@@ -0,0 +1,13 @@
+const { getChapters } = require("../database/db");
+
+const chapterInfo = async (request, response, next) => {
+    try {
+        const chaptersinfo = await getChapters();
+        response.status(200).json({ chaptersinfo });
+    } catch (error) {
+        console.log("[chapterInfo: Error]", error);
+        next(error);
+    }
+};
+
+module.exports = { chapterInfo };
