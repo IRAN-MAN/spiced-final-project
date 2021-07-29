596e69aaa0ca13ceecb97b9a66531bc3ab3c13f3
CHRISTO GOGLER <christogogler@gmail.com>
2021-07-29 15:44:24 +0200

    add Coauthors Comp

---
+++ client/src/components/CoauthorsList.js
@@ -0,0 +1,39 @@
+import { Link } from "react-router-dom";
+
+export default function CoauthorsList(props) {
+    console.log("...(CoauhtorsList) coauthors: ", props.coauthors);
+    const renderCoauthors = () => {
+        return props.coauthors.map((coauthor) => {
+            return (
+                <li key={coauthor.id} className="coauthorWrapper">
+                    <Link to={"/users/profile/" + coauthor.id}>
+                        <div className="miniAvatarWrapper">
+                            <img
+                                className="avatar smallAvatar"
+                                src={coauthor.profile_pic}
+                                alt={
+                                    coauthor.first_name +
+                                    " from " +
+                                    coauthor.city
+                                }
+                            />
+                            <button className="tooltip">
+                                <span className="tooltiptext">
+                                    {coauthor.first_name}
+                                </span>
+                            </button>
+                        </div>
+                    </Link>
+                </li>
+            );
+        });
+    };
+
+    return (
+        <div className="coauthorsListWrapper">
+            <ul className="flex jcc frow">
+                {props.coauthors.length > 0 && renderCoauthors()}
+            </ul>
+        </div>
+    );
+}
