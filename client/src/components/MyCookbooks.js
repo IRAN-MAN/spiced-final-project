433dd70175e38183fe90b0ac08e9417185526ec3
CHRISTO GOGLER <christogogler@gmail.com>
2021-07-30 14:13:52 +0200

    Gallery implemented and debugged on MyCookbooks

--- client/src/components/MyCookbooks.js
+++ client/src/components/MyCookbooks.js
@@ -66,7 +66,7 @@ export default function MyCookbooks() {
             <ul>
                 <Gallery
                     elements={cookbooksTEST}
-                    elementsPerPage={1}
+                    elementsPerPage={2}
                     render={renderCookbooks}
                 />
             </ul>
