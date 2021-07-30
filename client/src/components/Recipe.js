eac5080399786ea5d5ed6226b69158617dc5e0fc
CHRISTO GOGLER <christogogler@gmail.com>
2021-07-30 22:59:13 +0200

    add name, story, ingrdients, instructions

--- client/src/components/Recipe.js
+++ client/src/components/Recipe.js
@@ -39,6 +39,45 @@ export default function Recipe(props) {
                         </span>
                     </p>
                 </div>
+                <div className="ingredientsWrapper">
+                    <p className="ingredients">
+                        <span className="bolder">Ingredients:</span>INSERT
+                        INGREDIENTS HERE!
+                    </p>
+                </div>
+                <div className="instructionsWrapper">
+                    <div className="instructions">
+                        <p>
+                            Lorem ipsum dolor sit amet, consectetuer adipiscing
+                            elit, sed diam nonummy nibh euismod tincidunt ut
+                            laoreet dolore magna aliquam erat volutpat. Ut wisi
+                            enim ad minim veniam, quis nostrud exerci tation
+                            ullamcorper suscipit lobortis nisl ut aliquip ex ea
+                            commodo consequat. Duis autem vel eum iriure dolor
+                            in hendrerit in vulputate velit esse molestie
+                            consequat, vel illum dolore eu feugiat nulla
+                            facilisis at vero eros et accumsan et iusto odio
+                            dignissim qui blandit praesent luptatum zzril
+                            delenit augue duis dolore te feugait nulla facilisi.
+                            Nam liber tempor cum soluta nobis eleifend option
+                            congue nihil imperdiet doming id quod mazim placerat
+                            facer possim assum.Lorem ipsum dolor sit amet,
+                            consectetuer adipiscing elit, sed diam nonummy nibh
+                            euismod tincidunt ut laoreet dolore magna aliquam
+                            erat volutpat. Ut wisi enim ad minim veniam, quis
+                            nostrud exerci tation ullamcorper suscipit lobortis
+                            nisl ut aliquip ex ea commodo consequat. Duis autem
+                            vel eum iriure dolor in hendrerit in vulputate velit
+                            esse molestie consequat, vel illum dolore eu feugiat
+                            nulla facilisis at vero eros et accumsan et iusto
+                            odio dignissim qui blandit praesent luptatum zzril
+                            delenit augue duis dolore te feugait nulla facilisi.
+                            Nam liber tempor cum soluta nobis eleifend option
+                            congue nihil imperdiet doming id quod mazim placerat
+                            facer possim assum.
+                        </p>
+                    </div>
+                </div>
             </div>
         </div>
     );
