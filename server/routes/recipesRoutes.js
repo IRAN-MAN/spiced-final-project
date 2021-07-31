cc777762b0a17eaef73feb214770d4eba3adbc8e
Milad <khoshkaran.milad@gmail.com>
2021-07-31 11:53:58 +0200

    recipeRoutes - S3 and uploader middleware added

--- server/routes/recipesRoutes.js
+++ server/routes/recipesRoutes.js
@@ -2,13 +2,25 @@ const express = require("express");
 const recipesRoutes = express.Router();
 
 const errorHandler = require("../middlewares/errorHandler");
+const { s3Upload } = require("../utilities/S3_config");
+const { uploader } = require("../utilities/uploader_config");
 
 const {
     recipeInfo,
     recipesInCookBook,
+    addRecipeInCookBook,
+    addRecipePhoto,
 } = require("../middlewares/recipesHandler");
 
 recipesRoutes.get("/:recipe_id", recipeInfo, errorHandler);
 recipesRoutes.get("/by_cookbook/:cookbook_id", recipesInCookBook, errorHandler);
+recipesRoutes.post("/add_recipe", addRecipeInCookBook, errorHandler);
+recipesRoutes.post(
+    "/add_recipe_photo/:recipe_id",
+    uploader.single("file"),
+    s3Upload,
+    addRecipePhoto,
+    errorHandler
+);
 
 module.exports = { recipesRoutes };
