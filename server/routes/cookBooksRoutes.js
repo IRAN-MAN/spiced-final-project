12521245d00335ee65be59cf21d017038dd63362
Milad <khoshkaran.milad@gmail.com>
2021-07-31 12:19:51 +0200

    cookBookRoutes - S3 and Uploader middleware added

--- server/routes/cookBooksRoutes.js
+++ server/routes/cookBooksRoutes.js
@@ -2,11 +2,14 @@ const express = require("express");
 const cookBooksRoutes = express.Router();
 
 const errorHandler = require("../middlewares/errorHandler");
+const { s3Upload } = require("../utilities/S3_config");
+const { uploader } = require("../utilities/uploader_config");
 
 const {
     cookBookInfo,
     createNewCookBook,
     updateCookBookInfo,
+    updateCookBookCover,
 } = require("../middlewares/cookBooksHandler");
 
 cookBooksRoutes.get("/:user_id", cookBookInfo, errorHandler);
@@ -14,5 +17,12 @@ cookBooksRoutes.get("/:user_id", cookBookInfo, errorHandler);
 cookBooksRoutes.post("/create_cookbook", createNewCookBook, errorHandler);
 
 cookBooksRoutes.put("/update_info", updateCookBookInfo, errorHandler);
+cookBooksRoutes.put(
+    "/update_cover",
+    uploader.single("file"),
+    s3Upload,
+    updateCookBookCover,
+    errorHandler
+);
 
 module.exports = { cookBooksRoutes };
