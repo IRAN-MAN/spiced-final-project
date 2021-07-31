433539380a42972cf31ce87a2f090bbbbab47c40
CHRISTO GOGLER <christogogler@gmail.com>
2021-07-31 15:19:31 +0200

    add IngredientsList component

---
+++ client/src/components/IngredientsList.js
@@ -0,0 +1,31 @@
+export default function IngredientsList(props) {
+    const renderIngredients = () => {
+        console.log(
+            "...(IngredientsList renderIngredients) ",
+            props.ingredients_list
+        );
+        return props.ingredients_list.map((ingredient) => {
+            console.log("ingredient:", ingredient);
+            const ingredientresult = (
+                <span className="ingredient" key={ingredient.id}>
+                    {ingredient.quantity}
+                    {ingredient.unit} {ingredient.ingredient_name},{" "}
+                </span>
+            );
+            // console.log(
+            //     "...(IngredientsList renderIngredients) result: ",
+            //     ingredientresult
+            // );
+            return ingredientresult;
+        });
+    };
+
+    return (
+        <div className="ingredientsWrapper flex cc">
+            <p className="ingredients">
+                <span className="bolder">Ingredients: </span>
+                {renderIngredients()}
+            </p>
+        </div>
+    );
+}
