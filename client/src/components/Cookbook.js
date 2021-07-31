d7e7e7b98dcb70074ab6ecf7032be3f78847052a
CHRISTO GOGLER <christogogler@gmail.com>
2021-07-31 11:13:52 +0200

    remove testdata

--- client/src/components/Cookbook.js
+++ client/src/components/Cookbook.js
@@ -1,12 +1,7 @@
 import { useSelector, useDispatch } from "react-redux";
 import { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
 
-import {
-    receiveCoauthors,
-    receiveRecipes,
-    storeCurrentCookbook,
-} from "../redux/action-creators";
+import { receiveCoauthors, receiveRecipes } from "../redux/action-creators";
 
 //components
 import CoauthorsList from "./CoauthorsList";
@@ -28,14 +23,6 @@ export default function Cookbook(props) {
     useEffect(() => {
         setCurrentCookbook(cookbooks[cookbook_id]);
     }, [cookbooks]);
-    const currentCookbookTEST = {
-        cookbook_name: "Pasta Dreams",
-        author: "Sally Salamander",
-        cover_pic:
-            "https://images.unsplash.com/photo-1546549032-9571cd6b27df?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=668&q=80",
-    };
-    // const chaptersTEST = ["Starters", "Main", "Desert"];
-
     return (
         <div className="profileWrapper flex cc fcolumn">
             Cookbook Component
