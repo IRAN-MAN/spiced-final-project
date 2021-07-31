173cda3a98597e7d23e00e900ca697d35ed3738a
CHRISTO GOGLER <christogogler@gmail.com>
2021-07-31 15:33:26 +0200

    removed addrecipe button

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -12,9 +12,6 @@ import axios from "../axios";
 
 import { BrowserRouter, Link, Redirect, Route, Switch } from "react-router-dom";
 
-//hooks
-import { useToggle } from "../hooks/hooks";
-
 // components
 import Footer from "./Footer";
 import Header from "./Header";
@@ -23,15 +20,13 @@ import MyProfile from "./MyProfile";
 import Test from "./Test";
 import Cookbook from "./Cookbook";
 import Recipe from "./Recipe";
-import Button from "./Button";
-import AddRecipe from "./AddRecipe";
+
 // import UserProfile from "./UserProfile";
 
 export default function App() {
     //useSelectors here:
     const user = useSelector((state) => state.user);
     const dispatch = useDispatch();
-    const [toggle, toggleOnOff] = useToggle();
 
     //useEffects here:
     useEffect(async () => {
@@ -70,14 +65,6 @@ export default function App() {
                 </Switch>
             </div>
             <Footer />
-            <Button
-                onClick={() => toggleOnOff(false)}
-                labeltext="add recipe"
-                type="submit"
-                classNames="button edit-button"
-                icon="edit"
-            />
-            {toggle && <AddRecipe toggle={toggle} toggleOnOff={toggleOnOff} />}
         </BrowserRouter>
     );
 }
