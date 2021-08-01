e4aedf6d36f8ff527d9f59af6ca594016de44eaf
CHRISTO GOGLER <christogogler@gmail.com>
2021-08-01 23:13:46 +0200

    add selector/dispatch for currentcookbook

--- client/src/components/Cookbook.js
+++ client/src/components/Cookbook.js
@@ -4,7 +4,9 @@ import { useEffect, useState } from "react";
 import {
     receiveCoauthors,
     receiveRecipes,
+    receiveCurrentCookbook,
     populateCurrentCookbook,
+    uploadCookbookCover,
 } from "../redux/action-creators";
 
 //components
@@ -12,6 +14,7 @@ import CoauthorsList from "./CoauthorsList";
 import ChapterList from "./ChapterList";
 import Button from "./Button";
 import AddRecipe from "./AddRecipe";
+import UploadPictureForm from "./forms/UploadPictureForm";
 
 //hooks
 import { useToggle } from "../hooks/hooks";
@@ -19,25 +22,32 @@ import { useToggle } from "../hooks/hooks";
 export default function Cookbook(props) {
     const dispatch = useDispatch();
     const cookbook_id = props.match.params.id;
-    const [currentCookbook, setCurrentCookbook] = useState({});
+    // const [currentCookbook, setCurrentCookbook] = useState({});
     const coauthors = useSelector((state) => state.coauthors);
     const cookbooks = useSelector((state) => state.cookbooks);
+    const currentCookbook = useSelector((state) => state.currentCookbook);
+
     const [toggle, toggleOnOff] = useToggle();
 
-    const currentCB = cookbooks.filter((x) => x.cookbook_id == cookbook_id);
+    // const currentCB = cookbooks.filter((x) => x.cookbook_id == cookbook_id);
 
     useEffect(() => {
+        dispatch(receiveCurrentCookbook(cookbook_id));
+        dispatch(receiveCoauthors(cookbook_id));
         if (cookbook_id != undefined) {
             dispatch(receiveRecipes(cookbook_id));
         }
-        dispatch(receiveCoauthors(cookbook_id));
     }, []);
 
-    useEffect(() => {
-        setCurrentCookbook(currentCB);
-        console.log("[currentCookbook]", currentCB);
-        dispatch(populateCurrentCookbook(currentCB));
-    }, [cookbooks]);
+    // useEffect(() => {
+    //     // setCurrentCookbook(currentCB);
+    //     // console.log("[currentCookbook]", currentCB);
+    //     //dispatch(populateCurrentCookbook(currentCB));
+    // }, [cookbooks]);
+
+    // useEffect(() => {
+    // }, [currentCookbook]);
+
     return (
         <div className="profileWrapper flex cc fcolumn">
             Cookbook Component
@@ -50,6 +60,11 @@ export default function Cookbook(props) {
                     />
                 </div>
             )}
+            <UploadPictureForm
+                label="add photo"
+                action={uploadCookbookCover}
+                id={cookbook_id}
+            />
             {currentCookbook && (
                 <div className="bioContent">
                     <h1>{currentCookbook.cookbook_name}</h1>
