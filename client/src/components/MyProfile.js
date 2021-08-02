b0633a5e9f396ab0130fb5fb51ab2df8ba9c01de
CHRISTO GOGLER <christogogler@gmail.com>
2021-08-02 07:22:09 +0200

    add UploadProfilePic

--- client/src/components/MyProfile.js
+++ client/src/components/MyProfile.js
@@ -1,8 +1,10 @@
 import { useSelector, useDispatch } from "react-redux";
 import { useEffect } from "react";
+import { uploadProfilePic } from "../redux/action-creators";
 
 //components
 import MyCookbooks from "./MyCookbooks";
+import UploadPictureForm from "./forms/UploadPictureForm";
 
 export default function MyProfile() {
     const user = useSelector((state) => {
@@ -23,6 +25,11 @@ export default function MyProfile() {
                     alt={user.first_name + " " + user.last_name}
                 />
             </div>
+            <UploadPictureForm
+                label="add photo"
+                action={uploadProfilePic}
+                id={user.id}
+            />
             <div className="bioContent">
                 <h1>{user.first_name + " " + user.last_name}</h1>
                 <div>
