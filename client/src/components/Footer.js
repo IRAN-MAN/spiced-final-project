ad983aaed1edfd11d580176c005a03fc0ba3d8b9
CHRISTO GOGLER <christogogler@gmail.com>
2021-08-03 00:36:39 +0200

    change text

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -3,7 +3,8 @@ export default function Footer() {
         <footer className="flex cc">
             <div className="flex fcolumn cc">
                 <p>What`s Coooking Good-Looking</p>
-                <p>© Christo Gogler + Milad Khoshkaran @ SPICED 2021</p>
+                <p>© Christo Gogler + Milad Khoshkaran </p>
+                <p>@ SPICED 2021</p>
             </div>
         </footer>
     );
