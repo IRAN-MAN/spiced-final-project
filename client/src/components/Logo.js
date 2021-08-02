e7c0826c70fb1c2cabe14dd8f0eaab95e86c9c10
CHRISTO GOGLER <christogogler@gmail.com>
2021-08-03 00:32:49 +0200

    add small logo

--- client/src/components/Logo.js
+++ client/src/components/Logo.js
@@ -1,12 +1,12 @@
 const LOGO_URL = "/images/logo.png";
-const LOGO_URL_SMALL = "https://via.placeholder.com/100";
+const LOGO_URL_SMALL = "/images/logosmall.png";
 
 export default function Logo() {
     return (
         <picture className="logo">
             <source media="(min-width: 768px)" srcSet={LOGO_URL} />
-
-            <img className="logo" src="" alt="" />
+            <source media="(min-width: 480px)" srcSet={LOGO_URL_SMALL} />
+            <img className="logo" src={LOGO_URL_SMALL} alt="LOGO" />
         </picture>
     );
 }
