a6e0b4dc4f01abaf9edfd505776c3a6ff5af961d
CHRISTO GOGLER <christogogler@gmail.com>
2021-08-02 13:32:46 +0200

    add nav, change layout, add logo

--- client/src/components/Welcome.js
+++ client/src/components/Welcome.js
@@ -2,12 +2,29 @@
 import Login from "./Login";
 import Registration from "./Registration";
 import { BrowserRouter, Link, Redirect, Route, Switch } from "react-router-dom";
+import Logo from "./Logo";
 
 export default function Welcome() {
     return (
         <BrowserRouter>
-            <div>
-                Welcome Component
+            <div className="flex fcolumn cc">
+                <div>
+                    <Logo />
+                </div>
+
+                <div className="navWrapper">
+                    <nav>
+                        <ul className="nav-ul flex vcenter">
+                            <li>
+                                <Link to="/login">Login</Link>
+                            </li>
+                            <li>
+                                <Link to="/">Sign up</Link>
+                            </li>
+                        </ul>
+                    </nav>
+                </div>
+
                 <Switch>
                     <Route exact path="/" render={() => <Registration />} />
                     <Route path="/login" component={Login} />
