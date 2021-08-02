f3f226f26dbb3f9ea2b2cde5e00810374c73eaba
CHRISTO GOGLER <christogogler@gmail.com>
2021-08-03 00:45:52 +0200

    show owner as first author

--- client/src/components/CoauthorsList.js
+++ client/src/components/CoauthorsList.js
@@ -2,6 +2,28 @@ import { Link } from "react-router-dom";
 
 export default function CoauthorsList(props) {
     console.log("...(CoauhtorsList) coauthors: ", props.coauthors);
+    const user = props.user;
+    const renderOwner = () => {
+        return (
+            <li key={user.coauthor_id} className="coauthorWrapper">
+                <Link to={"/users/profile/" + user.id}>
+                    <div className="miniAvatarWrapper">
+                        <div className="tooltip">
+                            <span className="tooltiptext">
+                                {user.first_name}
+                            </span>
+                            <img
+                                className="avatar smallAvatar"
+                                src={user.profile_pic}
+                                alt={user.first_name + " from " + user.city}
+                            />
+                        </div>
+                    </div>
+                </Link>
+            </li>
+        );
+    };
+
     const renderCoauthors = () => {
         return props.coauthors.map((coauthor) => {
             return (
@@ -32,6 +54,7 @@ export default function CoauthorsList(props) {
     return (
         <div className="coauthorsListWrapper">
             <ul className="flex jcc vcenter frow">
+                {renderOwner()}
                 {props.coauthors.length > 0 && renderCoauthors()}
                 <li className="coauthorWrapper">
                     <div className="miniAvatarWrapper">
