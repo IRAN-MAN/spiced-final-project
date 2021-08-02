4ed0e4d8d80dce332750eecd487874fd080835a1
CHRISTO GOGLER <christogogler@gmail.com>
2021-08-03 00:33:28 +0200

    change classnames

--- client/src/components/Recipe.js
+++ client/src/components/Recipe.js
@@ -61,7 +61,7 @@ export default function Recipe(props) {
     };
 
     return (
-        <div className="recipeWrapper flex cc fcolumn">
+        <div className="profileWrapper flex cc fcolumn">
             <div className="avatarWrapper">
                 <img
                     className="avatar"
