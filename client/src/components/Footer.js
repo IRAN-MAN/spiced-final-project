deb5158b0248e0395fdb7cf847ec10f414576e88
CHRISTO GOGLER <christogogler@gmail.com>
2021-08-03 10:27:03 +0200

    quick fix

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -2,7 +2,7 @@ export default function Footer() {
     return (
         <footer className="flex cc">
             <div className="flex fcolumn cc">
-                <p>What`s Coooking Good-Looking</p>
+                <p>What&apos;s Coooking Good-Looking</p>
                 <p>© Christo Gogler + Milad Khoshkaran </p>
                 <p>@ SPICED 2021</p>
             </div>
