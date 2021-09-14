5b660d55d5770bcd5deb06e412219de70a4a1b8e
CHRISTO GOGLER <christogogler@gmail.com>
2021-09-14 12:07:47 +0200

    refactor

--- client/src/components/IngredientsList.js
+++ client/src/components/IngredientsList.js
@@ -1,21 +1,13 @@
 export default function IngredientsList(props) {
     const renderIngredients = () => {
-        // console.log(
-        //     "...(IngredientsList renderIngredients) ",
-        //     props.ingredients_list
-        // );
         return props.ingredients_list.map((ingredient) => {
-            // console.log("ingredient:", ingredient);
             const ingredientresult = (
                 <span className="ingredient" key={ingredient.id}>
                     {ingredient.quantity}
                     {ingredient.unit} {ingredient.ingredient_name},{" "}
                 </span>
             );
-            // console.log(
-            //     "...(IngredientsList renderIngredients) result: ",
-            //     ingredientresult
-            // );
+
             return ingredientresult;
         });
     };
