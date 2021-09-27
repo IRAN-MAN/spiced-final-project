cbfbb44b3d797541042c940bc0188fda88096ec1
CHRISTO GOGLER <christogogler@gmail.com>
2021-09-27 18:58:03 +0200

    add fadein effect

--- client/src/components/InviteCoauthors.js
+++ client/src/components/InviteCoauthors.js
@@ -1,18 +1,21 @@
 //components
+import FadeIn from "./FadeIn";
 import InvitationForm from "./forms/InvitationForm";
 
 export default function InviteCoauthors(props) {
     const { toggleOnOff } = props;
 
     return (
-        <section className="backdrop">
-            <button
-                className="closeBackdropButton"
-                onClick={() => toggleOnOff(true)}
-            >
-                ×
-            </button>
-            <InvitationForm />
-        </section>
+        <FadeIn delay={250} duration={450}>
+            <section className="backdrop">
+                <button
+                    className="closeBackdropButton"
+                    onClick={() => toggleOnOff(true)}
+                >
+                    ×
+                </button>
+                <InvitationForm />
+            </section>
+        </FadeIn>
     );
 }
