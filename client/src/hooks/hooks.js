4bc123d8e9704fad1387cb19c519e47f894d417a
CHRISTO GOGLER <christogogler@gmail.com>
2021-09-27 18:52:50 +0200

    add hook useCurrentWidth

--- client/src/hooks/hooks.js
+++ client/src/hooks/hooks.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import axios from "../axios";
 
 export function useStatefulFields() {
@@ -48,4 +48,36 @@ export function useAddToIngredients() {
         ingredients.push(ingredient);
     };
     return [ingredients, addIngredient];
+}
+
+export function useCurrentWidth() {
+    const getWidth = () =>
+        window.innerWidth ||
+        document.documentElement.clientWidth ||
+        document.body.clientWidth;
+    // save current window width in the state object
+    let [width, setWidth] = useState(getWidth());
+
+    // in this case useEffect will execute only once because
+    // it does not have any dependencies.
+    useEffect(() => {
+        // timeoutId for debounce mechanism
+        let timeoutId = null;
+        const resizeListener = () => {
+            // prevent execution of previous setTimeout
+            clearTimeout(timeoutId);
+            // change width from the state object after 150 milliseconds
+            timeoutId = setTimeout(() => setWidth(getWidth()), 150);
+        };
+        // set resize listener
+        window.addEventListener("resize", resizeListener);
+
+        // clean up function
+        return () => {
+            // remove resize listener
+            window.removeEventListener("resize", resizeListener);
+        };
+    }, []);
+
+    return width;
 }
