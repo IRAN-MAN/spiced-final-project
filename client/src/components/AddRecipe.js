b6917aa9776110467b2a771d824eb804489ceb48
CHRISTO GOGLER <christogogler@gmail.com>
2021-09-27 18:53:58 +0200

    add fadein effect

--- client/src/components/AddRecipe.js
+++ client/src/components/AddRecipe.js
@@ -1,18 +1,21 @@
 //components
 import RecipeForm from "./forms/RecipeForm";
+import FadeIn from "./FadeIn";
 
 export default function AddRecipe(props) {
     const { toggleOnOff } = props;
 
     return (
         <section className="backdrop">
-            <button
-                className="closeBackdropButton"
-                onClick={() => toggleOnOff(true)}
-            >
-                ×
-            </button>
-            <RecipeForm toggleOnOff={toggleOnOff} />
+            <FadeIn delay={250} duration={850}>
+                <button
+                    className="closeBackdropButton"
+                    onClick={() => toggleOnOff(true)}
+                >
+                    ×
+                </button>
+                <RecipeForm toggleOnOff={toggleOnOff} />
+            </FadeIn>
         </section>
     );
 }
