1342296cba3bd4679a69ccb111ef02d1d3dfa59f
Christo Gogler <82805245+ChristoGogler@users.noreply.github.com>
2021-12-07 13:15:21 +0100

    add conditional heading/subline

--- client/src/components/elements/FormWrapper.js
+++ client/src/components/elements/FormWrapper.js
@@ -1,8 +1,8 @@
 export default function FormWrapper({ heading, subline, children }) {
     return (
-        <div>
-            <h2>{heading}</h2>
-            <p>{subline}</p>
+        <div className="flex fcolumn cc">
+            {heading && <h2>{heading}</h2>}
+            {subline && <p>{subline}</p>}
             {children}
         </div>
     );
