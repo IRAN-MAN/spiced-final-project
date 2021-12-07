caebec8b64d5fb1abb5106e557d4cea343d23883
Christo Gogler <82805245+ChristoGogler@users.noreply.github.com>
2021-12-07 13:39:30 +0100

    new TempIngredientsList comp

---
+++ client/src/components/forms/recipe/TempIngredientsList.js
@@ -0,0 +1,29 @@
+import { useDispatch } from "react-redux";
+import { deleteIngredientById } from "../../../redux/action-creators";
+
+export default function TempIngredientsList({ ingredients }) {
+    console.log("TempList:", ingredients);
+    const dispatch = useDispatch();
+
+    const deleteIngredient = (id) => {
+        dispatch(deleteIngredientById(id, ingredients));
+    };
+
+    const renderIngredients = () => {
+        return ingredients.map((ingredient, index) => {
+            return (
+                <li key={index}>
+                    {ingredient.quantity} {ingredient.unit}{" "}
+                    {ingredient.ingredient_name}
+                    <button onClick={() => deleteIngredient(index)}>×</button>
+                </li>
+            );
+        });
+    };
+
+    return (
+        <ul className="ingredientslist">
+            {ingredients.length > 0 && renderIngredients()}
+        </ul>
+    );
+}
